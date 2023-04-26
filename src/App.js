4445ff8547cf88a5394b94053b223307006d2a1f
OwenMcGirr <o.a.mcgirr@gmail.com>
2023-04-26 18:42:07 +0100

    Implement typing view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
+import TypingPage from './pages/typing/TypingPage';
 import SignInPage from './pages/auth-flow/SignInPage';
 import SignUpPage from './pages/auth-flow/SignUpPage';
 import ResetPasswordPage from './pages/auth-flow/ResetPasswordPage';
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/type" element={<TypingPage />} />
           <Route path="/sign-in" element={<SignInPage />} />
           <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
---
+++ src/pages/typing/TypingPage.js
@@ -0,0 +1,51 @@
+// This view allows the user to type what they want to say
+// It will contain the text that the user is typing in a text area
+// It will contain buttons to speak the text and clear the text
+// It will contain a button to save the text as a phrase
+
+import React from 'react';
+import BaseLayout from '../../layout/BaseLayout';
+import './TypingPage.css';
+
+function TypingPage() {
+    const [text, setText] = React.useState("");
+
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleClearText = () => {
+        setText("");
+    }
+
+    const handleSaveText = () => {
+        // TODO: Save the text as a phrase
+    }
+
+    const handleSpeakText = () => {
+        // TODO: Speak the text
+    }
+
+    return (
+        <BaseLayout>
+            <div className="container">
+                <textarea className="text-area" value={text} onChange={handleTextChange} />
+                {text.length > 0 && (
+                    <div className="button-container">
+                        <button className="btn btn-primary" onClick={handleSpeakText}>
+                            Speak
+                        </button>
+                        <button className="btn btn-primary" onClick={handleClearText}>
+                            Clear
+                        </button>
+                        <button className="btn btn-primary" onClick={handleSaveText}>
+                            Save
+                        </button>
+                    </div>
+                )}
+            </div>
+        </BaseLayout>
+    );
+}
+
+export default TypingPage;
