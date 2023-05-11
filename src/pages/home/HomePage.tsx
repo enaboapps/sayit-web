66fe87bdcfc6cb78d07f563a14ac1155e1bf56b4
OwenMcGirr <o.a.mcgirr@gmail.com>
2023-05-11 15:34:51 +0100

    Implement home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,18 +2,45 @@ import React from "react";
 import BaseLayout from "../../layout/BaseLayout";
 import Auth from "../../business-logic/backend/Auth";
 import { Link } from "react-router-dom";
+import "../../global.css";
+import './HomePage.css';
 
-class HomePage extends React.Component {
-    // Just render an empty div inside BaseLayout
-    render() {
-        return (
-            <BaseLayout>
-                <div>
-                    <h1>Home</h1>
-                </div>
-            </BaseLayout>
-        );
-    }
+// This is the home page of the app. It is the first page that the user sees when they visit the app.
+// If the user is signed in, they will see a welcome message and a grid of links to the other pages.
+// If the user is not signed in, they will see a message telling them to sign in.
+
+function HomePage() {
+    const signedIn = Auth.isSignedIn();
+    return (
+        <BaseLayout>
+            <div className="container">
+                <h1>Home</h1>
+                {signedIn ? (
+                    <>
+                        <p>Welcome, {Auth.getCurrentUserEmail()}</p>
+                        <div className="grid">
+                            <Link to="/type" className="card">
+                                <h3>Type</h3>
+                                <p>Type messages</p>
+                            </Link>
+                            <Link to="/boards" className="card">
+                                <h3>Phrases</h3>
+                                <p>View and edit your phrases</p>
+                            </Link>
+                            <Link to="/account" className="card">
+                                <h3>Account</h3>
+                                <p>View your account details</p>
+                            </Link>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <p>Please <Link to="/sign-in">sign in</Link> to continue.</p>
+                    </>
+                )}
+            </div>
+        </BaseLayout>
+    );
 }
 
 export default HomePage;
