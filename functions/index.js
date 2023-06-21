d21c4c9cdeac1294e80a750e06ea06a5a0c91659
OwenMcGirr <o.a.mcgirr@gmail.com>
2023-06-21 17:35:43 +0100

    Implement functions

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const { Configuration, OpenAIApi } = require('openai');
+
+exports.askOpenAI = functions.https.onRequest(async (req, res) => {
+    const configuration = new Configuration({
+        apiKey: functions.config().openai.key,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const question = req.body.question;
+    const generalSystemMessage = "You are inside an Augmentative and Alternative Communication system. Your job is to assist the user in communicating with others. You can do this by generating text for the user to send to others."
+
+    const messages = [
+        { role: "system", content: generalSystemMessage },
+        { role: "user", content: question }
+    ];
+
+    const chatCompletion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+    });
+
+    res.json({ answer: chatCompletion.data.choices[0].message });
+});
