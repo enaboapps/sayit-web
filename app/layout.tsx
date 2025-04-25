377c2cf1ec998327e15c39a074b9d526183647ee
OwenMcGirr <60785457+enaboapps@users.noreply.github.com>
2025-04-25 14:03:40 +0100

    PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="application-name" content="SayIt" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="SayIt" />
+        <meta name="description" content="A communication app for everyone" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>
         <AuthProvider>
           <SettingsProvider>
