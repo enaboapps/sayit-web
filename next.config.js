b0a2ccd62937ad7419f46cbb69e474e615f9667c
OwenMcGirr <60785457+enaboapps@users.noreply.github.com>
2025-04-26 13:36:55 +0100

    PWA fix

--- next.config.js
+++ next.config.js
@@ -10,55 +10,9 @@ const nextConfig = {
   },
 };
 
-const config = withPWA({
+export default withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'https-calls',
-        networkTimeoutSeconds: 15,
-        expiration: {
-          maxEntries: 150,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: /\/_next\/image\?url=.*/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'next-image',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'static-images',
-        expiration: {
-          maxEntries: 64,
-          maxAgeSeconds: 24 * 60 * 60, // 24 hours
-        },
-      },
-    },
-  ],
-  fallbacks: {
-    document: '/offline',
-  },
-  customWorkerDir: 'public',
-  customWorkerScript: 'custom-service-worker.js',
+  disable: process.env.NODE_ENV === 'development'
 })(nextConfig);
-
-export default config;
