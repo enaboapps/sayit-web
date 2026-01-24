c63abe34f28ecf5137412d0d0646047bbd159fc0
Owen McGirr <o.a.mcgirr@gmail.com>
2026-01-24 21:15:57 +0000

    Update generated Convex API types

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -10,6 +10,7 @@
 
 import type * as caregiverClients from "../caregiverClients.js";
 import type * as connectionRequests from "../connectionRequests.js";
+import type * as migrations from "../migrations.js";
 import type * as phraseBoards from "../phraseBoards.js";
 import type * as phrases from "../phrases.js";
 import type * as profiles from "../profiles.js";
@@ -26,6 +27,7 @@ import type {
 declare const fullApi: ApiFromModules<{
   caregiverClients: typeof caregiverClients;
   connectionRequests: typeof connectionRequests;
+  migrations: typeof migrations;
   phraseBoards: typeof phraseBoards;
   phrases: typeof phrases;
   profiles: typeof profiles;
