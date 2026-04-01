7b37ec0a204e8b59059d29fbbba8a16a19141d17
Owen McGirr <o.a.mcgirr@gmail.com>
2026-04-01 10:24:42 +0100

    chore: remove Clerk appearance customization (#441)
    The clerkDarkAppearance config wasn't working as intended.
    Remove the appearance file and prop from sign-in/sign-up pages,
    letting Clerk use its default styling.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
-import { clerkDarkAppearance } from '@/lib/clerkAppearance';
 
 export default function SignInPage() {
   return (
     <div className="min-h-screen bg-background flex items-center justify-center p-4">
-      <SignIn appearance={clerkDarkAppearance} />
+      <SignIn />
     </div>
   );
 }
