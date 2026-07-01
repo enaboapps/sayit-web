7f5ac958cafe2d857bc4cad9e7ca76ec23588934
Owen McGirr <o.a.mcgirr@gmail.com>
2026-07-01 21:30:48 +0100

    Update AAC processor dependency

---
+++ apps/web/lib/stubs/path-module.js
@@ -0,0 +1,11 @@
+export function join() {
+  throw new Error('path.join is not available in the browser bundle.');
+}
+
+export function dirname() {
+  throw new Error('path.dirname is not available in the browser bundle.');
+}
+
+export function basename() {
+  throw new Error('path.basename is not available in the browser bundle.');
+}
